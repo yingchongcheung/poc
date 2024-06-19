c6e896eda26c4b4cad3cb4fd661da65e57104db7
yingchongcheung <yingchong.cheung@student.hu.nl>
2024-06-19 12:10:46 +0200

    test terraform vpc

--- Templates/main.tf
+++ Templates/main.tf
@@ -33,86 +33,86 @@ module "vpc" {
         Environment = "dev"}
 }
 
-resource "aws_security_group" "MySG" {
-  name = "MySG"
-  description = "My Rules"
-  vpc_id = module.vpc.vpc_id
+# resource "aws_security_group" "MySG" {
+#   name = "MySG"
+#   description = "My Rules"
+#   vpc_id = module.vpc.vpc_id
 
-  ingress = {
-    name = "HTTP"
-    description = "Ingress HTTP from anywhere"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+#   ingress = {
+#     name = "HTTP"
+#     description = "Ingress HTTP from anywhere"
+#     from_port = 80
+#     to_port = 80
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
 
-    name = "ICMP"
-    description = "Ingress ICMP from anywhere"
-    from_port = 8
-    to_port = 0
-    protocol = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+#     name = "ICMP"
+#     description = "Ingress ICMP from anywhere"
+#     from_port = 8
+#     to_port = 0
+#     protocol = "icmp"
+#     cidr_blocks = ["0.0.0.0/0"]
 
-    name = "SSH"
-    description = "Ingress SSH from anywhere"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    }
-  } 
+#     name = "SSH"
+#     description = "Ingress SSH from anywhere"
+#     from_port = 22
+#     to_port = 22
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   } 
 
-  resource "aws_instance" "srv1" {
-    ami           = "ami-0440d3b780d96b29d"
-    instance_type = "t2.micro"
-    key_name = "vockey"
-    subnet_id = module.vpc.public_subnets[0] 
-    vpc_security_group_ids = [aws_security_group.MySG.id]
-    user_data = <<-EOF
-                #!/bin/bash
-                # Install Apache Web Server and PHP 
-                dnf install -y httpd wget php mariadb105-server 
-                # Download Lab files 
-                wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-100-ACCLFO-2/2
-                lab2-vpc/s3/lab-app.zip 
-                unzip lab-app.zip -d /var/www/html/ 
-                # Turn on web server 
-                chkconfig httpd on 
-                service httpd start
-                EOF
-  }
+  # resource "aws_instance" "srv1" {
+  #   ami           = "ami-0440d3b780d96b29d"
+  #   instance_type = "t2.micro"
+  #   key_name = "vockey"
+  #   subnet_id = module.vpc.public_subnets[0] 
+  #   vpc_security_group_ids = [aws_security_group.MySG.id]
+  #   user_data = <<-EOF
+  #               #!/bin/bash
+  #               # Install Apache Web Server and PHP 
+  #               dnf install -y httpd wget php mariadb105-server 
+  #               # Download Lab files 
+  #               wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-100-ACCLFO-2/2
+  #               lab2-vpc/s3/lab-app.zip 
+  #               unzip lab-app.zip -d /var/www/html/ 
+  #               # Turn on web server 
+  #               chkconfig httpd on 
+  #               service httpd start
+  #               EOF
+  # }
 
-  resource "aws_instance" "srv2" {
-    ami           = "ami-0440d3b780d96b29d"
-    instance_type = "t2.micro"
-    key_name = "vockey"
-    subnet_id = module.vpc.public_subnets[1]
-    vpc_security_group_ids = [aws_security_group.MySG.id]
-    user_data = <<-EOF
-                #!/bin/bash
-                # Install Apache Web Server and PHP 
-                dnf install -y httpd wget php mariadb105-server 
-                # Download Lab files 
-                wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-100-ACCLFO-2/2
-                lab2-vpc/s3/lab-app.zip 
-                unzip lab-app.zip -d /var/www/html/ 
-                # Turn on web server 
-                chkconfig httpd on 
-                service httpd start
-                EOF
-  }
+  # resource "aws_instance" "srv2" {
+  #   ami           = "ami-0440d3b780d96b29d"
+  #   instance_type = "t2.micro"
+  #   key_name = "vockey"
+  #   subnet_id = module.vpc.public_subnets[1]
+  #   vpc_security_group_ids = [aws_security_group.MySG.id]
+  #   user_data = <<-EOF
+  #               #!/bin/bash
+  #               # Install Apache Web Server and PHP 
+  #               dnf install -y httpd wget php mariadb105-server 
+  #               # Download Lab files 
+  #               wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-100-ACCLFO-2/2
+  #               lab2-vpc/s3/lab-app.zip 
+  #               unzip lab-app.zip -d /var/www/html/ 
+  #               # Turn on web server 
+  #               chkconfig httpd on 
+  #               service httpd start
+  #               EOF
+  # }
 
-  resource "aws_lb_target_group" "MyTargets" {
-    name = "MyTargets"
-    port = 80
-    protocol = "HTTP"
-    vpc_id = module.vpc.vpc_id
-  }
+  # resource "aws_lb_target_group" "MyTargets" {
+  #   name = "MyTargets"
+  #   port = 80
+  #   protocol = "HTTP"
+  #   vpc_id = module.vpc.vpc_id
+  # }
 
-  resource "aws_lb" "MyLB" {
-    name = "MyLB"
-    internal = false
-    load_balancer_type = "application"
-    security_groups = [aws_security_group.MySG.id]
-    subnets = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]] 
-  }
+  # resource "aws_lb" "MyLB" {
+  #   name = "MyLB"
+  #   internal = false
+  #   load_balancer_type = "application"
+  #   security_groups = [aws_security_group.MySG.id]
+  #   subnets = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]] 
+  # }
